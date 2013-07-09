b772e81afb005d876a40a2a66a6ece26880985a9
Brent Yorgey <byorgey@cis.upenn.edu>
2013-07-08 21:56:14 -0400

    more work on document explaining Active semantics

--- semantics/ActiveDiagrams.hs
+++ semantics/ActiveDiagrams.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -8,7 +10,9 @@ module ActiveDiagrams where
 import           Diagrams.Backend.Cairo
 import           Diagrams.Coordinates
 import           Diagrams.Prelude
+import           Graphics.SVGFonts.ReadFont
 
+timeline :: Double -> Double -> Diagram Cairo R2
 timeline t1 t2 =
        circle 0.2 # fc black
     <> (t1 & 0) ~~ (t2 & 0) # dashing [0.1,0.1] 0
@@ -33,10 +37,31 @@ instance Drawable XActive where
       drawLine Nothing  = mempty
       drawLine (Just x) = vrule 3 # lw 0.1 # translateX x
 
-xactive s e c = XActive
+xactive' :: Double -> Double -> Diagram Cairo R2 -> XActive
+xactive' s e d = XActive
   ( Just s
-  , rect (e - s) 2 # lw 0 # fcA (c `withOpacity` 0.5) # alignL # translateX s
+  , d
   , Just e
   )
+
+xactive :: Double -> Double -> Colour Double -> XActive
+xactive s e c = xactive' s e (xactiveRect s e c)
+
+xactiveRect :: Double -> Double -> Colour Double -> Diagram Cairo R2
+xactiveRect s e c
+  = rect (e - s) 2 # lw 0 # fcA (c `withOpacity` 0.5) # alignL # translateX s
+
+xactiveD :: Double -> Double -> Colour Double -> Diagram Cairo R2
+xactiveD s e c = draw (xactive s e c)
+
+a1, a2, a12 :: Diagram Cairo R2
+a1 = xactiveD (-6) 3 red
+a2 = xactiveD (-1) 5 blue
+a12 = xactiveD (-1) 3 purple
+
+tl :: Diagram Cairo R2
+tl = timeline (-10) 10
+
+text' s = (stroke $ textSVG' (TextOpts s lin2 INSIDE_H KERN False 4 4)) # fc black # lw 0
 
 -- main = defaultMain (timeline (-10) 10)
