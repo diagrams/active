cfa2ddaa003819f143f3c4f2f51a945efe5c166f
Brent Yorgey <byorgey@cis.upenn.edu>
2013-08-23 16:42:39 -0400

    Fun With Type-Level Proofs

--- src/Data/Active.hs
+++ src/Data/Active.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE EmptyDataDecls             #-}
@@ -10,6 +10,7 @@
 {-# LANGUAGE PolyKinds                  #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
@@ -23,6 +24,8 @@
 -----------------------------------------------------------------------------
 
 module Data.Active where
+
+import GHC.Exts
 
 import           Control.Applicative
 import           Control.Arrow       ((***))
@@ -43,6 +46,19 @@ data EndpointType
   = I -- nfinite
   | C -- losed
   | O -- pen
+
+data CompatPf (e1 :: EndpointType) (e2 :: EndpointType) where
+  CompatCO :: CompatPf C O
+  CompatOC :: CompatPf O C
+
+class Compat (e1 :: EndpointType) (e2 :: EndpointType) where
+  compat :: CompatPf e1 e2
+
+instance Compat C O where
+  compat = CompatCO
+
+instance Compat O C where
+  compat = CompatOC
 
 -- Convert Closed to Open
 type family Open (x :: EndpointType) :: EndpointType
@@ -67,6 +83,7 @@ and so on...?
 data Endpoint :: EndpointType -> * -> * where
   Infinity ::      Endpoint I t
   Finite   :: t -> Endpoint C t
+  FiniteO  :: t -> Endpoint O t
 
 deriving instance Show t => Show (Endpoint e t)
 deriving instance Eq t   => Eq   (Endpoint e t)
@@ -126,7 +143,7 @@ class Fractional a => FractionalOf v a where
 
 data Proxy a = Proxy
 
-class Clock t => Deadline (l :: EndpointType) (r :: EndpointType) t a where
+class (Clock t, Compat l r) => Deadline (l :: EndpointType) (r :: EndpointType) t a where
   -- choose deadline-time time-now (if before deadline) (if after deadline)
   choose :: Proxy l -> Proxy r -> t -> t -> a -> a -> a
 
@@ -247,9 +264,10 @@ instance AffineSpace t => Shifty (Endpoint e t) where
 --
 --   @Era@ is (intentionally) abstract. To construct @Era@ values, use
 --   'mkEra'; to deconstruct, use 'start' and 'end'.
-data Era :: EndpointType -> EndpointType -> * -> * where
-  EmptyEra :: Era C C t
-  Era      :: Endpoint l t -> Endpoint r t -> Era l r t
+data GenEra :: (EndpointType -> EndpointType -> Constraint)
+            -> EndpointType -> EndpointType -> * -> * where
+  EmptyEra :: con l r => GenEra con l r t
+  Era      :: Endpoint l t -> Endpoint r t -> GenEra con l r t
 
   -- We do not export the Era constructor, and maintain the invariant
   -- that the start time is always <= the end time.
@@ -257,19 +275,32 @@ data Era :: EndpointType -> EndpointType -> * -> * where
 deriving instance Show t => Show (Era l r t)
 deriving instance Eq   t => Eq   (Era l r t)
 
+data AreCPf :: EndpointType -> EndpointType -> * where
+  AreCPf :: AreCPf C C
+
+class AreC (l :: EndpointType) (r :: EndpointType) where
+  areC :: AreCPf l r
+
+instance AreC C C where
+  areC = AreCPf
+
+type Era  = GenEra AreC
+
+type SEra = GenEra Compat
 
 -- | Two eras intersect to form the largest era which is contained in
 --   both, with the empty era as an annihilator.
-eraIsect :: Ord t => Era l1 r1 t -> Era l2 r2 t -> Era (Isect l1 l2) (Isect r1 r2) t
+eraIsect :: forall l1 r1 l2 r2 t. Ord t => Era l1 r1 t -> Era l2 r2 t -> Era (Isect l1 l2) (Isect r1 r2) t
 eraIsect (Era l1 r1) (Era l2 r2) = canonicalizeEra $ Era (endpointMax l1 l2) (endpointMin r1 r2)
-eraIsect EmptyEra (Era Infinity Infinity)     = EmptyEra
-eraIsect EmptyEra (Era (Finite _) Infinity)   = EmptyEra
-eraIsect EmptyEra (Era Infinity (Finite _))   = EmptyEra
-eraIsect EmptyEra (Era (Finite _) (Finite _)) = EmptyEra
-eraIsect (Era Infinity Infinity)     EmptyEra = EmptyEra
-eraIsect (Era (Finite _) Infinity)   EmptyEra = EmptyEra
-eraIsect (Era Infinity (Finite _))   EmptyEra = EmptyEra
-eraIsect (Era (Finite _) (Finite _)) EmptyEra = EmptyEra
+eraIsect EmptyEra EmptyEra = case (areC :: AreCPf l1 r1, areC :: AreCPf l2 r2) of (AreCPf, AreCPf) -> EmptyEra
+eraIsect EmptyEra (Era Infinity Infinity)     = case (areC :: AreCPf l1 r1) of AreCPf -> EmptyEra
+eraIsect EmptyEra (Era (Finite _) Infinity)   = case (areC :: AreCPf l1 r1) of AreCPf -> EmptyEra
+eraIsect EmptyEra (Era Infinity (Finite _))   = case (areC :: AreCPf l1 r1) of AreCPf -> EmptyEra
+eraIsect EmptyEra (Era (Finite _) (Finite _)) = case (areC :: AreCPf l1 r1) of AreCPf -> EmptyEra
+eraIsect (Era Infinity Infinity)     EmptyEra = case (areC :: AreCPf l2 r2) of AreCPf -> EmptyEra
+eraIsect (Era (Finite _) Infinity)   EmptyEra = case (areC :: AreCPf l2 r2) of AreCPf -> EmptyEra
+eraIsect (Era Infinity (Finite _))   EmptyEra = case (areC :: AreCPf l2 r2) of AreCPf -> EmptyEra
+eraIsect (Era (Finite _) (Finite _)) EmptyEra = case (areC :: AreCPf l2 r2) of AreCPf -> EmptyEra
   -- ugh, all the above cases are actually needed to make the type checker happy
 
 -- Maintain the invariant that s <= e
@@ -281,6 +312,10 @@ canonicalizeEra era = era
 --   and is an annihilator for 'eraIsect'.
 emptyEra :: Era C C t
 emptyEra = EmptyEra
+
+-- | The era of ALL TIME
+allTime :: Era I I t
+allTime = Era Infinity Infinity
 
 -- | Check if an era is the empty era.
 eraIsEmpty :: Ord t => Era l r t -> Bool
@@ -320,8 +355,8 @@ instance AffineSpace t => Shifty (Era l r t) where
 --   track whether the left and right ends of the @Era@ are
 --   respectively infinite (@I@) or finite (@F@).  @Active_@ values
 --   may be combined via parallel composition; see 'par_'.
-data Active_ l r t a = Active_
-  { _era       :: Era l r t
+data Active_ era l r t a = Active_
+  { _era       :: era l r t
   , _runActive :: t -> a
   }
   deriving (Functor)
@@ -329,24 +364,24 @@ data Active_ l r t a = Active_
 makeLenses ''Active_
 
 -- | Create a bi-infinite, constant 'Active_' value.
-pure_ :: Ord t => a -> Active_ I I t a
-pure_ a = Active_ mempty (pure a)
+pure_ :: Ord t => a -> Active_ Era I I t a
+pure_ a = Active_ allTime (pure a)
 
 -- | \"Apply\" an 'Active_' function to an 'Active_' value, pointwise
 --   in time, taking the intersection of their intervals.  This is
 --   like '<*>' but with a richer indexed type.
 app_ :: Ord t
-     => Active_ l1 r1 t (a -> b)
-     -> Active_ l2 r2 t a
-     -> Active_ (Isect l1 l2) (Isect r1 r2) t b
-app_ (Active_ e1 f1) (Active_ e2 f2) = Active_ (e1 <> e2) (f1 <*> f2)
+     => Active_ Era l1 r1 t (a -> b)
+     -> Active_ Era l2 r2 t a
+     -> Active_ Era (Isect l1 l2) (Isect r1 r2) t b
+app_ (Active_ e1 f1) (Active_ e2 f2) = Active_ (eraIsect e1 e2) (f1 <*> f2)
 
 -- | Parallel composition of 'Active_' values.  The 'Era' of the
 --   result is the intersection of the 'Era's of the inputs.
 par_ :: (Semigroup a, Ord t)
-     => Active_ l1 r1 t a -> Active_ l2 r2 t a
-     -> Active_ (Isect l1 l2) (Isect r1 r2) t a
-par_ (Active_ e1 f1) (Active_ e2 f2) = Active_ (e1 <> e2) (f1 <> f2)
+     => Active_ Era l1 r1 t a -> Active_ Era l2 r2 t a
+     -> Active_ Era (Isect l1 l2) (Isect r1 r2) t a
+par_ (Active_ e1 f1) (Active_ e2 f2) = Active_ (eraIsect e1 e2) (f1 <> f2)
 
 -- par_ p1 p2 = pure_ (<>) `app_` p1 `app_` p2
 --   for the above to typecheck, would need to introduce a type-level proof
@@ -354,8 +389,8 @@ par_ (Active_ e1 f1) (Active_ e2 f2) = Active_ (e1 <> e2) (f1 <> f2)
 --   can also do:
 -- par_ p1 p2 = unsafeConvert_ $ pure_ (<>) `app_` p1 `app_` p2
 
-instance (Shifty a, AffineSpace t, t ~ ShiftyTime a) => Shifty (Active_ l r t a) where
-  type ShiftyTime (Active_ l r t a) = t
+instance (Shifty a, AffineSpace t, t ~ ShiftyTime a) => Shifty (Active_ Era l r t a) where
+  type ShiftyTime (Active_ Era l r t a) = t
 
   shift d = (runActive %~ shift d) . (era %~ shift d)
 
@@ -373,7 +408,7 @@ instance (Shifty a, AffineSpace t, t ~ ShiftyTime a) => Shifty (Active_ l r t a)
 --   'Active_', can actually be an instance of 'Applicative',
 --   'Semigroup', and 'Monoid'.
 data Active t a where
-  Active :: Active_ l r t a -> Active t a
+  Active :: Active_ Era l r t a -> Active t a
 
 -- | Apply a function at all times.
 instance Functor (Active t) where
@@ -405,6 +440,21 @@ instance (Shifty a, AffineSpace t, t ~ ShiftyTime a) => Shifty (Active t a) wher
   shift d (Active a) = Active (shift d a)
 
 ------------------------------------------------------------
+-- SEra
+------------------------------------------------------------
+
+eraSeq :: forall l1 r1 l2 r2 t.
+  Compat r1 l2 => SEra l1 r1 t -> SEra l2 r2 t -> SEra l1 r2 t
+eraSeq era1 era2 =
+  case (compat :: CompatPf r1 l2) of
+    CompatOC -> case era1 of
+      EmptyEra -> case (compat :: CompatPf l1 r1) of
+        CompatCO -> case era2 of
+          EmptyEra -> EmptyEra
+          _        -> era2
+    -- XXX a ton of other cases go here.
+
+------------------------------------------------------------
 -- SActive
 ------------------------------------------------------------
 
@@ -423,7 +473,7 @@ data Anchor = Start | End | Fixed
 --   @SActive@ values may be combined via sequential composition; see
 --   'seqL' and 'seqR'.  This is why the name is prefixed with @S@:
 --   think of it as \"Sequential Active\".
-data SActive l r t a = SActive { _active_ :: Active_ l r t a
+data SActive l r t a = SActive { _active_ :: Active_ SEra l r t a
                                , _anchors :: AnchorMap t
                                }
 
@@ -444,7 +494,7 @@ makeLenses ''SActive
 -- would not even be able to perform sequential composition on it --
 -- it would be pretty useless.
 
-float_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ l r t a -> SActive l r t a
+float_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ Era l r t a -> SActive l r t a
 float_ a_ = addDefaultAnchors $ SActive a_ M.empty
 
 addDefaultAnchors :: (AffineSpace t, VectorSpace (Diff t)) => SActive l r t a -> SActive l r t a
@@ -459,23 +509,23 @@ defaultAnchors (Era s e) = M.unions [startAnchor s, endAnchor e]
     endAnchor   (Finite e') = M.singleton End e'
     endAnchor   _           = M.empty
 
-floatR_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ l r t a -> SActive l (Open r) t a
+floatR_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ Era l r t a -> SActive l (Open r) t a
 floatR_ = openR . float_
 
-floatL_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ l r t a -> SActive (Open l) r t a
+floatL_ :: (AffineSpace t, VectorSpace (Diff t)) => Active_ Era l r t a -> SActive (Open l) r t a
 floatL_ = openL . float_
 
 openR :: SActive l r t a -> SActive l (Open r) t a
-openR = unsafeConvertS
+openR = undefined -- unsafeConvertS
 
 openL :: SActive l r t a -> SActive (Open l) r t a
-openL = unsafeConvertS
+openL = undefined -- unsafeConvertS
 
 closeR :: Eq t => a -> SActive l O t a -> SActive l C t a
-closeR = unsafeCloseS end
+closeR = undefined -- unsafeCloseS end
 
 closeL :: Eq t => a -> SActive O r t a -> SActive C r t a
-closeL = unsafeCloseS start
+closeL = undefined -- unsafeCloseS start
 
 unsafeCloseS = undefined
 -- unsafeCloseS :: Eq t
@@ -489,21 +539,21 @@ unsafeCloseS = undefined
 --     Just (Finite t') -> SActive (Active_ e (\t -> if t == t' then a else f t)) m
 
 -- ugggggghhhh
-(...) :: forall l1 r1 l2 r2 t a. (AffineSpace t, Deadline r1 l2 t a)
-    => SActive l1 r1 t a -> SActive l2 r2 t a -> SActive l1 r2 t a
-SActive (Active_ EmptyEra _) _ ... sa2 = unsafeConvertS sa2
-sa1 ... SActive (Active_ EmptyEra _) _ = unsafeConvertS sa1
-(...)
-  (SActive (Active_ (Era s1 (Finite e1)) f1) m1)
-  (SActive (Active_ (Era (Finite s2) e2) f2) m2)
-  = SActive (Active_ (Era s1 (shift d e2))
-                     (\t -> choose (Proxy :: Proxy r1) (Proxy :: Proxy l2)
-                              e1 t (f1 t) (shift d f2 t))
-            )
-            (combineAnchors m1 (shift d m2))
-  where
-    d = e1 .-. s2
-_ ... _ = error "... : impossible"
+-- (...) :: forall l1 r1 l2 r2 t a. (AffineSpace t, Deadline r1 l2 t a)
+--     => SActive l1 r1 t a -> SActive l2 r2 t a -> SActive l1 r2 t a
+-- SActive (Active_ EmptyEra _) _ ... sa2 = unsafeConvertS sa2
+-- sa1 ... SActive (Active_ EmptyEra _) _ = unsafeConvertS sa1
+-- (...)
+--   (SActive (Active_ (Era s1 (Finite e1)) f1) m1)
+--   (SActive (Active_ (Era (Finite s2) e2) f2) m2)
+--   = SActive (Active_ (Era s1 (shift d e2))
+--                      (\t -> choose (Proxy :: Proxy r1) (Proxy :: Proxy l2)
+--                               e1 t (f1 t) (shift d f2 t))
+--             )
+--             (combineAnchors m1 (shift d m2))
+--   where
+--     d = e1 .-. s2
+-- _ ... _ = error "... : impossible"
 
 combineAnchors :: AnchorMap t -> AnchorMap t -> AnchorMap t
 combineAnchors = M.unionWithKey select
@@ -513,7 +563,7 @@ combineAnchors = M.unionWithKey select
     select End   _ e = e
 
 instance Deadline r l t a => Semigroup (SActive l r t a) where
-  (<>) = (...)
+  (<>) = undefined -- (...)
 
 instance Deadline r l t a => Monoid (SActive l r t a) where
   mappend = (<>)
