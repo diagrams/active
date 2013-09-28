06b134e072f9d1e21c2f20f05412948b40185de8
Brent Yorgey <byorgey@cis.upenn.edu>
2013-09-27 23:59:19 -0400

    finish Pythagoras example

--- example/Pythagoras.hs
+++ example/Pythagoras.hs
@@ -1,36 +1,31 @@
 {-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE MultiParamTypeClasses     #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE ViewPatterns              #-}
 
 import           Control.Newtype
 import           Data.Active.Time
 import qualified Data.Map                       as M
 import           Data.Maybe                     (fromJust)
+import           Data.Monoid.Coproduct          (untangle)
+import           Data.Monoid.Split              (unsplit)
+import           Data.Typeable
 import           Diagrams.Backend.Cairo.CmdLine
 import           Diagrams.Coordinates
 import           Diagrams.Prelude
-
-keyframe :: ( OrderedField (Scalar v), HasLinearMap v, InnerSpace v
-              , Semigroup m )
-           => QDiagram b v m -> QDiagram b v m -> QAnimation b v m
-keyframe d1 d2 = f <$> durValued (dur 1)
-  where
-    pairs = concat . map strength . M.assocs
-          $ M.intersectionWith zip (unpack $ subMap d1) (unpack $ subMap d2)
-    strength (a,fb) = fmap ((,) a) fb
-    f t = mconcat
-        . map (\(_,(x1,x2)) ->
-                getSub x1
-                # translate
-                  ( lerp zeroV (location x2 .-. location x1)
-                    (fromJust $ sigma `atTime` t)
-                  )
-              )
-        $ pairs
+import           Diagrams.TwoD.Vector
+import           Graphics.SVGFonts
 
 type Anim = Animation Cairo R2
 type Dia = Diagram Cairo R2
+
+data TriLabel = TA | TB | TC
+  deriving (Eq, Ord, Show, Typeable)
+
+instance IsName TriLabel
 
 theTri :: Dia
 theTri
@@ -40,7 +35,7 @@ theTri
     # fromOffsets
     # closeTrail
     # (onLine . onLineSegments) (\[a,b,c] -> [b,c,a])
-    # strokeTrail
+    # strokeTrail' with {vertexNames = [[TC, TA, TB]]}
     # lw 0.05
     # lineJoin LineJoinRound
     # named "theTri"
@@ -60,8 +55,23 @@ ts = closeL mempty $ accumulate (replicate 3 (uopenL swoopY))
 movingTri :: Anim
 movingTri = transform <$> ts <*~> theTri
 
+fade :: (Functor f, HasStyle b) => f Double -> b -> f b
+fade f dia = opacity <$> f <*~> dia
+
 fadeIn :: Duration -> Dia -> Anim
-fadeIn d dia = opacity <$> sigma *>> fromDuration d <*~> dia
+fadeIn d = fade (sigma *>> fromDuration d)
+
+fadeOut :: Duration -> Dia -> Anim
+fadeOut d = fade (backwards sigma *>> fromDuration d)
+
+inout
+  = fade
+    ( movie
+      [ sigma *>> 0.5
+      , dur 1 $> 1
+      , backwards sigma *>> 0.5
+      ]
+    )
 
 canvas :: Dia
 canvas = square 15 # fc white # alignBL
@@ -79,22 +89,155 @@ triColumn
       # zipWith named [0::Int ..]
       # cat' unitY with {sep=1}
 
-square1
-  = theTri
-  # iterateN 4 (rotateAbout (3.5 & 3.5) (1/4 :: Turn))
-  # zipWith named [0::Int ..]
-  # mconcat
+parAL a1 a2 = ufree (anchorL 0 a1 `parA` anchorL 0 a2)
 
-scene1 :: Anim
-scene1
+bigT = theTri # scale 2 # centerOn canvas
+
+showTri
+  = movie
+    [ dur 1 $> bigT
+    , inout
+        ( mconcat
+          [ edgeLabel TC TA "b" bigT
+          , edgeLabel TA TB "c" bigT
+          , edgeLabel TB TC "a" bigT
+          ]
+        )
+      `parAL` (dur 10 $> bigT)
+    , dur 1 $> bigT
+    ]
+
+edgeLabel :: TriLabel -> TriLabel -> String -> Dia -> Dia
+edgeLabel v1 v2 label
+  = withNames [v1,v2] $ \subs ->
+      let [p1,p2] = map location subs
+          pmid    = alerp p1 p2 0.5
+          off     = perp (p2 .-. p1) # negateV # normalized
+          labelPt = pmid .+^ off
+      in
+          const (text' 2 label # moveTo labelPt)
+
+triToPosition :: Anim
+triToPosition = keyframe (const True) bigT (triColumn # atLm) *>> 2
+
+makeCopies :: Anim
+makeCopies
   = movie
     [ dur 1                  $> theTri # translate (1 & 1)
     , triColumn *>> (1/2)
     , dur 1                  $> atRm triColumn
     ]
 
-move1 = keyframe (atRm triColumn) square1 *>> 4
+square1
+  = replicate 4 theTri
+  # zipWith named [0::Int ..]
+  # zipWith transform (iterateN 4 (rotateAbout (3.5 & 3.5) (1/4 :: Turn)) mempty)
+  # mconcat
+  # centerOn canvas
+
+copiesToSquare
+  = keyframe (`elem` (map toName [0::Int .. 3])) (atRm triColumn) square1 *>> 4
+
+showC
+  = (dur 100 $> atRm copiesToSquare)
+    `parAL`
+    inout (text' 2 "c²" # centerOn canvas)
+
+config2
+  = (bl ||| juxtapose unitY bl tr) # fc green # centerOn canvas
+  where bl = theTri # fc green # named (1::Int) # rotateBy (1/4) # alignL
+          <> theTri # named (3::Int) # rotateBy (-1/4) # alignB
+        tr = theTri # named (0::Int) # alignT
+          <> theTri # named (2::Int) # rotateBy (1/2) # alignL
+
+squareToConfig2
+  = keyframe (`elem` (map toName [0::Int .. 3]))
+      square1 config2 # fc green *>> 3
+
+showAB
+  = (dur 100 $> atRm squareToConfig2)
+    `parAL`
+    inout
+      ( mconcat
+        [ text' 2 "a²" # centerOn canvas # translate ((-2.5) & 2.5)
+        , text' 2 "b²" # centerOn canvas # translate (1 & (-1))
+        ]
+      )
+
+pythagoras :: Anim
+pythagoras
+  = movie
+    [ showTri
+    , triToPosition
+    , makeCopies
+    , copiesToSquare
+    , dur 1 $> atRm copiesToSquare
+    , showC
+    , dur 1 $> atRm copiesToSquare
+    , squareToConfig2
+    , dur 1 $> atRm squareToConfig2
+    , showAB
+    , dur 1 $> atRm squareToConfig2
+    ]
 
 main :: IO ()
--- main = defaultMain square1
-main = animMain ((<>) <$> move1 <*~> canvas)
+-- main = defaultMain config2
+main = animMain ((<>) <$> pythagoras <*~> canvas)
+
+------------------------------------------------------------
+
+-- TODO: add this to std lib
+instance VectorSpace Turn where
+  type Scalar Turn = Double
+  d *^ (Turn t)    = Turn (d*t)
+
+interpolateT2 :: T2 -> T2 -> Double -> T2
+interpolateT2 t1 t2 s =
+  mconcat
+  [ translation (lerp p p' s)
+  , rotation (s *^ theta)
+  , scalingY (lerp 1 (magnitude uy') s)
+  , scalingX (lerp 1 (magnitude ux') s)
+  , translation (negateV p)
+  ]
+  where
+    tr :: (Transformable t, V t ~ R2) => t -> t
+    tr  = transform (t2 <> inv t1)
+    ux' = tr unitX
+    uy' = tr unitY
+    p   = transl t1
+    p'  = transl t2
+    theta :: Turn
+    theta = smallest (angleBetween unitX ux')
+    smallest (Turn phi)
+      | phi >= 1/2 = Turn $ phi - 1
+      | otherwise = Turn phi
+
+keyframe :: (Semigroup m)
+         => (Name -> Bool) -> QDiagram b R2 m -> QDiagram b R2 m -> QAnimation b R2 m
+keyframe namePred d1 d2 = f <$> durValued (dur 1)
+  where
+    pairs = concat . map strength . filter (namePred . fst) . M.assocs
+          $ M.intersectionWith zip (unpack $ subMap d1) (unpack $ subMap d2)
+    strength (a,fb) = fmap ((,) a) fb
+    f t = mconcat
+        . map (\(n
+                , ( x1@(Subdiagram _ (option mempty untangle -> (unsplit -> t1, _), _))
+                  ,    (Subdiagram _ (option mempty untangle -> (unsplit -> t2, _), _))
+                  )
+                )
+                ->
+
+                getSub x1
+                # transform (interpolateT2 t1 t2 (fromJust $ sigma `atTime` t))
+--                # named n  How to keep the names around?
+              )
+        $ pairs
+
+centerOn
+  :: (Alignable b, HasOrigin b, Enveloped a, V a ~ R2, V b ~ R2) =>
+     a -> b -> b
+centerOn c d = d # centerXY # moveTo (center2D c)
+
+text' :: Renderable (Path R2) b => Double -> String -> Diagram b R2
+text' d s = (stroke $ textSVG' (TextOpts s lin2 INSIDE_H KERN False d d)) # fc black # lw 0
