3976b0e7bfd5c3e83a828bbc7164cd640261e12c
Nicolas Wu <nicolas.wu@gmail.com>
2020-04-29 15:59:01 +0100

    add: some more laws

--- test/Laws.hs
+++ test/Laws.hs
@@ -42,6 +42,9 @@ cut c . cut d = cut (min c d)
 
 cut (c + d) x = cut c x ->- cut d (omit c x)    {c <= getDuration x}
 
+cut d x = x
+  -- if d >= getDuration x
+
 slice a b x
   | a <= b    = cut (b - a) (omit a x)
   | otherwise = backwards (slice b a x)
@@ -52,3 +55,28 @@ stretch (-1) x = backwards x
 stretch (j*k) = stretch j . stretch k   {j, k /= 0}
 
 snapshot t x = always (runActive t x)
+
+delay d x = lasting d mempty ->- x
+
+lasting d x = cut d (always x)
+
+(x ->- y) ->- z = x ->- (y ->- z)
+instant mempty ->- y = y
+x ->- instant mempty = x
+
+backwards (x ->- y) = backwards y ->- backwards x
+  -- if we have a commutative semigroup
+
+omit (getDuration x) (x ->> y) = y
+  -- if x finite
+
+omit Forever x = empty
+  -- provisional: we have no notion of empty
+
+cut (getDuration x) x ->> y = y
+
+cut d (x ->> y) = x ->> cut (d - getDuration x) y
+  -- if d >= getDuration x
+
+cut d (x ->> y) = x
+  -- if d < getDuration x
