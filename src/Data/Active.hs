aa6cfd791002bc89c41470bf86a749cfdedb126f
Brent Yorgey <byorgey@gmail.com>
2022-08-15 12:50:25 -0500

    fix warning: add `TypeOperators` for TH-generated lens code

--- src/Data/Active.hs
+++ src/Data/Active.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE TypeSynonymInstances       #-}
 {-# LANGUAGE UndecidableInstances       #-}
 {-# LANGUAGE ViewPatterns               #-}
